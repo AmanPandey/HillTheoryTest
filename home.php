c67b2ac51152e2624cda30768f5b3d473a0ab650
cpandeysoftgame <86830961+cpandeysoftgame@users.noreply.github.com>
2022-03-29 13:12:25 +0530

    Media query css fixed

--- home.php
+++ home.php
@@ -9,24 +9,81 @@
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
   <link href="css/custom.css" rel="stylesheet" />
+
+<!-- Media Query CSS -->
+  <style>
+    
+ .showMobile{
+   display: none;
+ }
+@media only screen and (max-width:767px){
+  body{
+    background-color: #0b1119;
+    width: 100%;
+  }
+
+  .hideMobile{
+      display: none;
+  }
+
+  .showMobile{
+    display: block;
+  }
+
+ .mobileScreen{
+ margin-left: 5px;
+ }
+
+
+ .scrollmenu {
+    overflow:  auto;
+    white-space: nowrap;
+    margin-left: 5px;
+    scrollbar-color: rebeccapurple green; 
+  }
+
+  .scrollItem{
+  width: 150px;
+  height: 150px;
+  background-color: #28292b;
+  border-radius: 10px;
+  display: inline-block;
+  margin-right: 5px;
+  }
+
+
+}
+  </style>
+
+<!-- /Media Query CSS -->
+
+
 </head>
 <body style="background-color: #0b1119;">
 
 
 <nav class="navbar" style="background-color:#0b1119;">
   <!-- Brand/logo -->
-  <a class="navbar-brand" href="#"><img src="assets/web_logo.png" alt="" width="200"></a>
+  <a class="navbar-brand" href="home"><img src="assets/web_logo.png" alt="" width="200"></a>
   <!-- Links -->
-  <ul style=" float:right;">
+  <ul style=" float:right;" class="hideMobile" >
     <button class="payment_btn">Payment</button>
     <Select><option value=""> Venkata Sai Teja M2</option></Select>
   </ul>
+
+  <ul style=" float:right;" class="showMobile">
+  <a href="orders"><img src="assets/menu.png" width="50" alt=""></a>
+  </ul>
+
+
 </nav>
+
+
 
 
 <nav class="navbar" style="background-color:transparent; margin-top:-30PX; font-size: 16px; width: 100%; ">
 <!-- Links -->
-  <span class="navbar" style="width: 100%;">
+  <span class="navbar hideMobile" style="width: 100%;">
     
   <div style="width: 50%;">
     <span class="nav-item" style="float:left; margin-right: 10px;">
@@ -56,7 +113,13 @@
 
   </span>
  
-    
+  <span class="navbar showMobile" style="width: 100%;">
+  <Small style="font-size: 10px; color: #f9b13a; float: left; margin-left: 50%; margin-right: 15px;" >EXPOSER <br/>  <span style="color:#fff;"> ₹ 0 </span></Small>
+  
+  <Small style="font-size: 10px; color: #f9b13a; float: right; margin-right: 5px;">Venkata Sai Teja M2 <br/>	<span style="color:#fff;"> ₹ 4,39,567.89  </span></Small>
+
+  
+  </span>
  
 </nav>
 
@@ -66,7 +129,8 @@
 <div class="row" style="margin-top: -20px;">
   <div class="col-sm-12">
 <hr style="height:2px; background-color:#646970;" />
-<h6 style="color: #c35d47; margin-left: 50px;">Top Gainers<br/> <small style="color: #646970;"> Coins that have gained the most in 24 hours </small> </h6>
+<h6 style="color: #c35d47; margin-left: 50px;" class="hideMobile">Top Gainers<br/> <small style="color: #646970;"> Coins that have gained the most in 24 hours </small> </h6>
+<h6 style="color: #c35d47; margin-left: 5px;" class="showMobile">Top Gainers<br/> <small style="color: #646970;"> Coins that have gained the most in 24 hours </small> </h6>
 
 <div class="scrollmenu" style="color:aliceblue;">
 
@@ -277,7 +341,8 @@
 <div class="row" style="margin-top: -10px;">
   <div class="col-sm-12">
 <hr style="height:2px; background-color:#646970;" />
-<h6 style="color: #c35d47; margin-left: 50px;">Top Loser<br/> <small style="color: #646970;"> Coins that have gained the most in 24 hours </small> </h6>
+<h6 style="color: #c35d47; margin-left: 50px;" class="hideMobile" >Top Loser<br/> <small style="color: #646970;"> Coins that have gained the most in 24 hours </small> </h6>
+<h6 style="color: #c35d47; margin-left: 5px;" class="showMobile" >Top Loser<br/> <small style="color: #646970;"> Coins that have gained the most in 24 hours </small> </h6>
 
 <div class="scrollmenu" style="color:aliceblue;">
 
@@ -317,7 +382,8 @@
 <div class="row" style="margin-top: -10px;">
   <div class="col-sm-12">
 <hr style="height:2px; background-color:#646970;" />
-<h6 style="color: #c35d47; margin-left: 50px;">Popular<br/> <small style="color: #646970;"> Most popular coins bought by first time investors </small> </h6>
+<h6 style="color: #c35d47; margin-left: 50px;" class="hideMobile" >Popular<br/> <small style="color: #646970;"> Most popular coins bought by first time investors </small> </h6>
+<h6 style="color: #c35d47; margin-left: 5px;" class="showMobile" >Popular<br/> <small style="color: #646970;"> Most popular coins bought by first time investors </small> </h6>
 
 <div class="scrollmenu" style="color:aliceblue;">
 
--- orders.php
+++ orders.php
@@ -6,59 +6,9 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
   <link href="css/custom.css" rel="stylesheet" />
-</head>
-<body style="background-color: #0b1119;">
 
-
-<nav class="navbar" style="background-color:#0b1119;">
-  <!-- Brand/logo -->
-  <a class="navbar-brand" href="#"><img src="assets/web_logo.png" alt="" width="200"></a>
-  <!-- Links -->
-  <ul style=" float:right;">
-    <button class="payment_btn">Payment</button>
-    <Select><option value=""> Venkata Sai Teja M2</option></Select>
-  </ul>
-</nav>
-
-
-
-<nav class="navbar" style="background-color:transparent; margin-top:-30PX; font-size: 16px; width: 100%; ">
-<!-- Links -->
-  <span class="navbar" style="width: 100%;">
-    
-  <div style="width: 50%;">
-    <span class="nav-item" style="float:left; margin-right: 10px;">
-      <a class="#" style="color: #646970;" href="home" >Home</a>
-    </span>
-    <span class="nav-item" style="float: left; margin-right: 10px;">
-      <a class="#" style="color: #646970;" href="#">Trades</a>
-    </span>
-    <span class="nav-item" style="float: left; margin-right: 10px;">
-      <a class="#" style="color: #646970;" href="#">Prices</a>
-    </span>
-    <span class="nav-item" style="float: left; margin-right: 10px;">
-      <a class="#" style="color: #646970;" href="#">Portfolio</a>
-    </span>
-    <span class="nav-item" style="float: left; margin-right: 10px;">
-      <a class="#" style="color: #c35d47;" href="orders">Orders</a>
-    </span>
-    
-  </div>
-
-    <div  style="width: 50%;">
-    
-  <Small style="font-size: 10px; color: #f9b13a; float: right; margin-right: 10px;">Venkata Sai Teja M2 <br/>	<span style="color:#fff;"> ₹ 4,39,567.89  </span></Small>
-
-  <Small style="font-size: 10px; color: #f9b13a; float: right;  margin-right: 50px;" >EXPOSER <br/>  <span style="color:#fff;"> ₹ 0 </span></Small>
-    </div>
-
-  </span>
- 
-    
- 
-</nav>
-
-<style>
+  <!-- Media Query CSS -->
+  <style>
   .tradeButton{
     width: 350px;
     height: 30px;
@@ -115,7 +65,127 @@
     border: 0px solid transparent;
     border-bottom: 2px solid #2a8ae0;
   }
-</style>
+
+
+
+ .showMobile{
+   display: none;
+ }
+@media only screen and (max-width:767px){
+  body{
+    background-color: #0b1119;
+    width: 100%;
+  }
+
+  .hideMobile{
+      display: none;
+  }
+
+  .showMobile{
+    display: block;
+  }
+
+
+  .tradeButton{
+    width: 120px;
+    height: 30px;
+    border: 2px solid #eb5029;
+    color: #fff;
+    background-color: #eb5029;
+    border-radius: 30px;
+    font-size: 16px;
+  }
+
+  .buySell{
+    background-color: transparent;
+    border-color: #646970;
+  }
+
+  .searchInputDate{
+    width: 100%;
+    background-color: transparent;
+    color: #646970;
+    border: 0px solid transparent;
+    border-bottom: 2px solid #2a8ae0;
+  }
+ 
+
+
+}
+  </style>
+
+<!-- /Media Query CSS -->
+
+
+</head>
+<body style="background-color: #0b1119;">
+
+
+<nav class="navbar" style="background-color:#0b1119;">
+  <!-- Brand/logo -->
+  <a class="navbar-brand" href="home"><img src="assets/web_logo.png" alt="" width="200"></a>
+  <!-- Links -->
+  <ul style=" float:right;" class="hideMobile" >
+    <button class="payment_btn">Payment</button>
+    <Select><option value=""> Venkata Sai Teja M2</option></Select>
+  </ul>
+
+  <ul style=" float:right;" class="showMobile">
+  <a href="orders"><img src="assets/menu.png" width="50" alt=""></a>
+  </ul>
+
+
+</nav>
+
+
+
+<div class="showMobile" style="width: 100%; height: 70px; bottom: -20px; position: fixed; ">
+  <img src="assets/mobile_menu.png" width-="100%" alt="">
+</div>
+
+<nav class="navbar" style="background-color:transparent; margin-top:-30PX; font-size: 16px; width: 100%; ">
+<!-- Links -->
+  <span class="navbar hideMobile" style="width: 100%;">
+    
+  <div style="width: 50%;">
+    <span class="nav-item" style="float:left; margin-right: 10px;">
+      <a class="#" style="color: #c35d47;" href="home" >Home</a>
+    </span>
+    <span class="nav-item" style="float: left; margin-right: 10px;">
+      <a class="#" style="color: #646970;" href="#">Trades</a>
+    </span>
+    <span class="nav-item" style="float: left; margin-right: 10px;">
+      <a class="#" style="color: #646970;" href="#">Prices</a>
+    </span>
+    <span class="nav-item" style="float: left; margin-right: 10px;">
+      <a class="#" style="color: #646970;" href="#">Portfolio</a>
+    </span>
+    <span class="nav-item" style="float: left; margin-right: 10px;">
+      <a class="#" style="color: #646970;" href="orders">Orders</a>
+    </span>
+    
+  </div>
+
+    <div  style="width: 50%;">
+    
+  <Small style="font-size: 10px; color: #f9b13a; float: right; margin-right: 10px;">Venkata Sai Teja M2 <br/>	<span style="color:#fff;"> ₹ 4,39,567.89  </span></Small>
+
+  <Small style="font-size: 10px; color: #f9b13a; float: right;  margin-right: 50px;" >EXPOSER <br/>  <span style="color:#fff;"> ₹ 0 </span></Small>
+    </div>
+
+  </span>
+ 
+  <span class="navbar showMobile" style="width: 100%;">
+  <Small style="font-size: 10px; color: #f9b13a; float: left; margin-left: 50%; margin-right: 15px;" >EXPOSER <br/>  <span style="color:#fff;"> ₹ 0 </span></Small>
+  
+  <Small style="font-size: 10px; color: #f9b13a; float: right; margin-right: 5px;">Venkata Sai Teja M2 <br/>	<span style="color:#fff;"> ₹ 4,39,567.89  </span></Small>
+
+  
+  </span>
+ 
+</nav>
+
+
 
 
 <div style="clear: both;"></div>
@@ -123,11 +193,109 @@
 <center>
   <button class="tradeButton" style="margin-right: 50px;" onclick="tradeHistory()">Trades History</button>
   <button class="tradeButton buySell"  onclick="buySell()">Buy/Sell</button>
-  
+
 
   <div style="clear: both;"></div>
-<table class="orderTable" style="border-collapse: separate; border-spacing: 0 1em;">
-<tr>
+
+  <div class="showMobile" style="width:90%">
+
+    <small style="color:#3c914f; margin-left: 10px;"> Search By Date</small>
+    <br>
+    <input style="margin-left: 10px;" type="date" class="searchInputDate" >
+  </div>
+
+
+<div style="width: 90%; margin-top: 20px;  background-color: #28292b; border-radius: 10px; color: #fff; ">
+  
+    <span style="margin-top:10px; float: left;"><img src="assets/doge.png" width="30" alt=""> DOGE (DOGE) 
+    <small style="color: #b03444;">Sell</small></span>
+  <br>
+  <div style=" margin-left: 10px;"><br>
+  <h6><small style="color: #90938a; float: left;">Market Value</small> <small style="color: #fff; float: right; margin-right: 20px;">₹8.55</small></h6><br>
+  <h6><small style="color: #90938a; float: left;">Amount</small> <small style="color: #fff; float: right; margin-right: 20px;">₹8.55</small></h6><br>
+  <h6><small style="color: #90938a; float: left;">Qty</small> <small style="color: #fff; float: right; margin-right: 20px;">₹8.55</small></h6><br>
+  <h6><small style="color: #90938a; float: left;">P/L</small> <small style="color: #fff; float: right; margin-right: 20px;">₹8.55</small></h6><br>
+  <h6><small style="color: #90938a; float: left;">Date</small> <small style="color: #fff; float: right; margin-right: 20px;">₹8.55</small></h6><br>
+  <h6><small style="color: #90938a; float: left;">P/L Percentage</small>  <small style="color: #fff; float: right; margin-right: 20px;">₹8.55</small></h6><br>
+  </div>
+
+</div>
+
+
+<div style="width: 90%; margin-top: 20px;  background-color: #28292b; border-radius: 10px; color: #fff; ">
+  
+    <span style="margin-top:10px; float: left;"><img src="assets/doge.png" width="30" alt=""> DOGE (DOGE) 
+    <small style="color: #b03444;">Sell</small></span>
+  <br>
+  <div style=" margin-left: 10px;"><br>
+  <h6><small style="color: #90938a; float: left;">Market Value</small> <small style="color: #fff; float: right; margin-right: 20px;">₹8.55</small></h6><br>
+  <h6><small style="color: #90938a; float: left;">Amount</small> <small style="color: #fff; float: right; margin-right: 20px;">₹8.55</small></h6><br>
+  <h6><small style="color: #90938a; float: left;">Qty</small> <small style="color: #fff; float: right; margin-right: 20px;">₹8.55</small></h6><br>
+  <h6><small style="color: #90938a; float: left;">P/L</small> <small style="color: #fff; float: right; margin-right: 20px;">₹8.55</small></h6><br>
+  <h6><small style="color: #90938a; float: left;">Date</small> <small style="color: #fff; float: right; margin-right: 20px;">₹8.55</small></h6><br>
+  <h6><small style="color: #90938a; float: left;">P/L Percentage</small>  <small style="color: #fff; float: right; margin-right: 20px;">₹8.55</small></h6><br>
+  </div>
+
+</div>
+
+
+<div style="width: 90%; margin-top: 20px;  background-color: #28292b; border-radius: 10px; color: #fff; ">
+  
+    <span style="margin-top:10px; float: left;"><img src="assets/doge.png" width="30" alt=""> DOGE (DOGE) 
+    <small style="color: #b03444;">Sell</small></span>
+  <br>
+  <div style=" margin-left: 10px;"><br>
+  <h6><small style="color: #90938a; float: left;">Market Value</small> <small style="color: #fff; float: right; margin-right: 20px;">₹8.55</small></h6><br>
+  <h6><small style="color: #90938a; float: left;">Amount</small> <small style="color: #fff; float: right; margin-right: 20px;">₹8.55</small></h6><br>
+  <h6><small style="color: #90938a; float: left;">Qty</small> <small style="color: #fff; float: right; margin-right: 20px;">₹8.55</small></h6><br>
+  <h6><small style="color: #90938a; float: left;">P/L</small> <small style="color: #fff; float: right; margin-right: 20px;">₹8.55</small></h6><br>
+  <h6><small style="color: #90938a; float: left;">Date</small> <small style="color: #fff; float: right; margin-right: 20px;">₹8.55</small></h6><br>
+  <h6><small style="color: #90938a; float: left;">P/L Percentage</small>  <small style="color: #fff; float: right; margin-right: 20px;">₹8.55</small></h6><br>
+  </div>
+
+</div>
+
+
+<div style="width: 90%; margin-top: 20px;  background-color: #28292b; border-radius: 10px; color: #fff; ">
+  
+    <span style="margin-top:10px; float: left;"><img src="assets/doge.png" width="30" alt=""> DOGE (DOGE) 
+    <small style="color: #b03444;">Sell</small></span>
+  <br>
+  <div style=" margin-left: 10px;"><br>
+  <h6><small style="color: #90938a; float: left;">Market Value</small> <small style="color: #fff; float: right; margin-right: 20px;">₹8.55</small></h6><br>
+  <h6><small style="color: #90938a; float: left;">Amount</small> <small style="color: #fff; float: right; margin-right: 20px;">₹8.55</small></h6><br>
+  <h6><small style="color: #90938a; float: left;">Qty</small> <small style="color: #fff; float: right; margin-right: 20px;">₹8.55</small></h6><br>
+  <h6><small style="color: #90938a; float: left;">P/L</small> <small style="color: #fff; float: right; margin-right: 20px;">₹8.55</small></h6><br>
+  <h6><small style="color: #90938a; float: left;">Date</small> <small style="color: #fff; float: right; margin-right: 20px;">₹8.55</small></h6><br>
+  <h6><small style="color: #90938a; float: left;">P/L Percentage</small>  <small style="color: #fff; float: right; margin-right: 20px;">₹8.55</small></h6><br>
+  </div>
+
+</div>
+
+
+<div style="width: 90%; margin-top: 20px;  background-color: #28292b; border-radius: 10px; color: #fff; ">
+  
+    <span style="margin-top:10px; float: left;"><img src="assets/doge.png" width="30" alt=""> DOGE (DOGE) 
+    <small style="color: #b03444;">Sell</small></span>
+  <br>
+  <div style=" margin-left: 10px;"><br>
+  <h6><small style="color: #90938a; float: left;">Market Value</small> <small style="color: #fff; float: right; margin-right: 20px;">₹8.55</small></h6><br>
+  <h6><small style="color: #90938a; float: left;">Amount</small> <small style="color: #fff; float: right; margin-right: 20px;">₹8.55</small></h6><br>
+  <h6><small style="color: #90938a; float: left;">Qty</small> <small style="color: #fff; float: right; margin-right: 20px;">₹8.55</small></h6><br>
+  <h6><small style="color: #90938a; float: left;">P/L</small> <small style="color: #fff; float: right; margin-right: 20px;">₹8.55</small></h6><br>
+  <h6><small style="color: #90938a; float: left;">Date</small> <small style="color: #fff; float: right; margin-right: 20px;">₹8.55</small></h6><br>
+  <h6><small style="color: #90938a; float: left;">P/L Percentage</small>  <small style="color: #fff; float: right; margin-right: 20px;">₹8.55</small></h6><br>
+  </div>
+
+</div>
+
+</center>
+
+
+  <center>
+
+<table class="orderTable hideMobile" style="border-collapse: separate; border-spacing: 0 1em;">
+<tr >
   <td colspan="3">
     <small style="color:#3c914f;"> Search By Market</small>
     <br>
@@ -139,6 +307,7 @@
     <input type="date" class="searchInputDate" >
   </td>
 </tr>
+
 
 <tr class="tablerow">
   <td class="tableColl" colspan="2"> 
@@ -222,6 +391,10 @@
 </table>
 
 <div style="clear: both;"></div>
+
+
+
+
 
 </center>
 
